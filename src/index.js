c8c2754296c4dcd77cfad42f1712cfd3037167a9
Jack Palfrey <jack.palf@gmail.com>
2019-07-30 14:34:56 +0100

    adding exit on error;

--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import { Consumer } from 'sqs-consumer'
 import bunyan from 'bunyan'
 
-const queueUrl = 'https://sqs.eu-west-1.amazonaws.com/account-id/queue-name'
 
 const logger = bunyan.createLogger({
   name: 'queue-drainer',
   serializers: bunyan.stdSerializers
 })
 
+const queueUrl = process.env.QUEUE || null;
+
+if (!queueUrl) {
+  logger.fatal({ queueUrl }, 'no queue url provided, please set QUEUE');
+  process.exit(1)
+}
+
 async function handleMessage (message) {
-  logger.info(message)
+  const messageId = message.MessageId
+
+  try {
+    const body = JSON.parse(message.Body)
+    logger.info({ messageId, body }, 'parsed json')
+  } catch (e) {
+    const body = message.Body
+    logger.info({ messageId, body }, 'received message')
+  }
 }
 
 const app = Consumer.create({ queueUrl, handleMessage })
 
 app.on('error', (err) => {
   logger.error({ err }, 'generic error')
+  process.exit(1);
 })
 
 app.on('processing_error', (err) => {
   logger.error({ err }, 'processing error')
+  process.exit(1);
 })
 
 app.start()
